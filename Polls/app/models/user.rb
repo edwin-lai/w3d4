3f8c7703565ff7515614e3d0ce31c34d8d315c6e
edwin-lai <edwinlai@ucla.edu>
2016-01-21 15:17:29 -0800

    finish completed polls method'

--- Polls/app/models/user.rb
+++ Polls/app/models/user.rb
@@ -18,4 +18,30 @@ class User < ActiveRecord::Base
     foreign_key: :user_id,
     primary_key: :id,
     class_name: 'Response'
+
+  def completed_polls
+    Poll.find_by_sql(<<-SQL)
+      SELECT
+        polls.*
+      FROM
+        polls
+      JOIN
+        questions ON questions.poll_id = polls.id
+      JOIN
+        answer_choices ON answer_choices.question_id = questions.id
+      LEFT OUTER JOIN (
+        SELECT
+          responses.*
+        FROM
+          responses
+        WHERE
+          responses.user_id = #{id}
+      ) AS user_responses
+        ON user_responses.answer_choice_id = answer_choices.id
+      GROUP BY
+        polls.id
+      HAVING
+        COUNT(questions.id) = COUNT(user_responses.id)
+    SQL
+  end
 end
